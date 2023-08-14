e48af5b37cb484d15ae0a646c3b6ce12a28bd74f
Dev <dev@optimus.local>
2023-08-14 21:52:53 +0200

    feat: poc ckeditor

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Welcome from '@/Components/Welcome';
 import AppLayout from '@/Layouts/AppLayout';
 
+
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '../Build/ckeditor';
+//import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 export default function Dashboard() {
+
+  let [data, setData] = useState('ok')
+
   return (
     <AppLayout
       title="Dashboard"
@@ -15,7 +23,32 @@ export default function Dashboard() {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <Welcome />
+            <div>ok</div>
+            {/* <Welcome /> */}
+
+            <CKEditor
+                    editor={ ClassicEditor }
+                    data={data}
+                    onChange={ ( event, editor ) => {
+                      const data = editor.getData();
+                      setData(data);
+                      console.log( { event, editor, data } );
+                  } }
+                    /* onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } } */
+                />
           </div>
         </div>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+import { createRequire } from 'node:module';
+const require = createRequire( import.meta.url );
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
+
+
 export default defineConfig({
   plugins: [
     laravel({
@@ -9,10 +14,17 @@ export default defineConfig({
       refresh: true,
     }),
     react(),
+    ckeditor5( { theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' ) } ),
   ],
   resolve: {
     alias: {
       '@': '/resources/js',
     },
   },
+  /* define: {
+    // Some libraries use the global object, even though it doesn't exist in the browser.
+    // Alternatively, we could add `<script>window.global = window;</script>` to index.html.
+    // https://github.com/vitejs/vite/discussions/5912
+    global: {},
+  }, */
 });
